1391a7073e385969d076b55790d38b3765defbcb
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2020-04-28 10:45:54 -0300

    Front and Back Connection

--- Frontend/expense-tracker/src/context/GlobalState.js
+++ Frontend/expense-tracker/src/context/GlobalState.js
@@ -32,12 +32,35 @@ export const GlobalProvider = ({ children }) => {
         }
     }
 
-    function deleteTransaction(id) {
-        dispatch({ type: "DELETE_TRANSACTION", payload: id });
+    async function deleteTransaction(id) {
+        try {
+            await axios.delete(`/transactions/${id}`);
+
+            dispatch({ type: "DELETE_TRANSACTION", payload: id });
+        } catch (err) {
+            dispatch({
+                type: "TRANSACTION_ERROR",
+                payload: err.response.data.error,
+            });
+        }
     }
 
-    function addTransaction(transaction) {
-        dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+    async function addTransaction(transaction) {
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        try {
+            const res = await axios.post("/transactions", transaction, config);
+            dispatch({ type: "ADD_TRANSACTION", payload: res.data.data });
+        } catch (err) {
+            dispatch({
+                type: "TRANSACTION_ERROR",
+                payload: err.response.data.error,
+            });
+        }
     }
 
     return (
