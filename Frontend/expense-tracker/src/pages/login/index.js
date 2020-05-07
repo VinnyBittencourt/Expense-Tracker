a9059da52cc815e0aa0c48866df4423f7dfec1d2
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2020-05-07 17:55:07 -0300

    Log In

--- Frontend/expense-tracker/src/pages/login/index.js
+++ Frontend/expense-tracker/src/pages/login/index.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom"; //Link é uma outra forma de usar a tag 'a' porque a tag a faz com que a página faça o load inteiro na página
 import { FiLogIn } from "react-icons/fi";
 
 import { Container } from "./style";
 
-export default function login() {
+import api from "../../services/api";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        const data = {
+            email,
+            password,
+        };
+
+        try {
+            const respon = await api.post("/auth/authenticate", data);
+
+            console.log(respon.data.user.name);
+            localStorage.setItem("nameUser", respon.data.user.name);
+            localStorage.setItem("IdUser", respon.data.user._id);
+
+            history.push("/tracker");
+        } catch (err) {
+            console.log(err);
+            alert("Something went wrong!");
+        }
+    }
     return (
         <Container>
             <h1>Expense Tracker</h1>
-            <form>
-                <input type='text' placeholder='Email' />
-                <input type='text' placeholder='password' />
+            <form onSubmit={handleLogin}>
+                <input
+                    type='text'
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <Link className='back-link' to='/register'>
                     <FiLogIn size={14} color='#9c88ff'></FiLogIn>
                     Don't have an account? Register now.
