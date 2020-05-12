ec78357f20e0ccac0f142ea943b7f8601b5f2026
Vinícius de Ataídes Bittencourt <vinisataides@gmail.com>
2020-05-12 19:13:20 -0300

    Route validation

--- Frontend/expense-tracker/src/pages/Tracker/index.js
+++ Frontend/expense-tracker/src/pages/Tracker/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { Header } from "../../components/Header";
 import { Balance } from "../../components/Balance";
 import { IncomeExpenses } from "../../components/IncomeExpenses";
@@ -8,6 +9,14 @@ import { AddTransaction } from "../../components/AddTransaction";
 import { GlobalProvider } from "../../context/GlobalState";
 
 export default function Tracker() {
+    const history = useHistory();
+
+    useEffect(() => {
+        const jwtmain = localStorage.getItem("JWT");
+        if (!jwtmain) {
+            history.push("/");
+        }
+    }, []);
     return (
         <GlobalProvider>
             <Header />
